3b5bcf5cb8174e2371c6d6a918edd2e7f50e434a
th-release <cth.release@gmail.com>
2023-02-23 03:56:08 +0900

    Create 2738.py

---
+++ 2738.py
@@ -0,0 +1,16 @@
+x, y = map(int, input().split())
+
+a, b = [], []
+
+for row in range(x):
+  row = list(map(int, input().split()))
+  a.append(row)
+
+for row in range(x):
+  row = list(map(int, input().split()))
+  b.append(row)
+
+for row in range(x):
+  for col in range(y):
+    print(a[row][col] + b[row][col], end=" ")
+  print()
