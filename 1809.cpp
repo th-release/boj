b7811091cef72472f6d5d2bd81999f1acd576bfc
th-release <cth.release@gmail.com>
2023-02-23 02:38:44 +0900

    Create 1809.cpp

---
+++ 1809.cpp
@@ -0,0 +1,18 @@
+/* 아... 코드 작성하니까 골프 스크립트만,,,
+(___)
+(o o)____/
+ @@      \
+  \ ____,/
+  //   //
+ ^^   ^^
+*/
+#include <iostream>
+
+int main() {
+  std::cout << "(___)" << std::endl;
+  std::cout << "(o o)____/" << std::endl;
+  std::cout << " @@      \\" << std::endl;
+  std::cout << "  \\ ____,/" << std::endl;
+  std::cout << "  //   //" << std::endl;
+  std::cout << " ^^   ^^" << std::endl;
+}
